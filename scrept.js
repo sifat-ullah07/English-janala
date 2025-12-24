4ed1013e34e4ca043d64116cb18fd84f9f00fecf
sifat-ullah07 <hmjamil95@gmail.com>
2025-12-24 23:49:56 +0600

    added card feture and vocabulary card

--- scrept.js
+++ scrept.js
@@ -3,6 +3,46 @@ const loadLessons = () => {
     .then(response => response.json())
     .then((json) => displayLesson(json.data) )
 }
+const loadLevelWord = (id) => {
+    // console.log(id)
+    const url = `https://openapi.programming-hero.com/api/level/${id}`
+    // console.log(url)
+    fetch(url)
+    .then(response => response.json())
+    .then((json) => displayLevelWord(json.data))
+
+}
+const displayLevelWord = (words) => {
+    // console.log(words)
+    const wordContainer = document.getElementById('word-container')
+    wordContainer.innerHTML = '';
+
+    // {
+    // "id": 52,
+    // "level": 3,
+    // "word": "Bewilder",
+    // "meaning": "বিভ্রান্ত করা",
+    // "pronunciation": "বিওয়াইল্ডার"
+    // }
+
+    words.forEach(word => {
+        console.log(word)
+        const card = document.createElement('div')
+        card.innerHTML = `        
+        <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
+            <h2 class="text-2xl font-bold">${word.word}</h2>
+            <p class="font-semibold">Meaning /Pronounciation</p>
+            <div class="text-2xl font-medium font-bangla">"${word.meaning} / ${word.pronunciation}"</div>
+            <div class="flex justify-between items-center">
+                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
+            </div>
+        </div>`
+        
+        wordContainer.append(card)
+    });
+
+}
 const displayLesson = (lessons) => {
     console.log(lessons)
     const levelContainer = document.getElementById('level-container')
@@ -10,7 +50,7 @@ const displayLesson = (lessons) => {
 
     for(let lesson of lessons) {
         const btnDiv = document.createElement('div')
-        btnDiv.innerHTML = `<button class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}
+        btnDiv.innerHTML = `<button onclick = "loadLevelWord( ${lesson.level_no})" class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}
         </button>`
 
         levelContainer.appendChild(btnDiv)
