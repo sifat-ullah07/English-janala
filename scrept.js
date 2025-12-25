cd30dde4d251bc2ddad5e6b52708dd58e78ac395
sifat-ullah07 <hmjamil95@gmail.com>
2025-12-25 16:44:47 +0600

    added navbar

--- scrept.js
+++ scrept.js
@@ -3,6 +3,12 @@ const createElements = (arr) => {
     const htmlElements = arr.map((Element) => `<span class = "btn"> ${Element}</span>`)
     return (htmlElements.join(" "));
 };
+
+function pronounceWord(word) {
+  const utterance = new SpeechSynthesisUtterance(word);
+  utterance.lang = "en-EN"; // English
+  window.speechSynthesis.speak(utterance);
+}
 const manageSpinner = (ststus) => {
     const manageSpinner = (status) => {  
     if (status === true) {
@@ -141,7 +147,7 @@ const displayLevelWord = (words) => {
             <div class="text-2xl font-medium font-bangla">"${word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি" } / ${word.pronunciation ? word.pronunciation : ' pronunciation পাওয়া যায়নি ' }"</div>
             <div class="flex justify-between items-center">
                 <button onclick = "loadWordDetail(${word.id})" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
-                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
+                <button onclick ="pronounceWord('${word.word}')" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
             </div>
         </div>`
         
